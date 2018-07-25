0cb8d3068340740422fc76a59ce929926903a11e
Eduard Mazo <eduard.mazo@gmail.com>
2018-07-25 10:12:36 -0500

    :whale:

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template lang="pug">
+  #app
+    .container
+      img(src="./assets/logo.png")
+    .container
+      router-view
+</template>
+
+<script>
+export default {
+  name: "App"
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
