9d25eed4a7c335a6dab0f8c310864f175b53443a
Eduard Mazo <eduard.mazo@gmail.com>
2018-08-09 14:37:10 -0500

    Adding background & styles

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,10 +1,9 @@
 <template lang="pug">
-.container
-    h1.title HOLA
-    p {{msg}}
-    router-link(:to="{name: 'login'}") login
-
-  
+.bg(id="bg-img")
+    .container
+        h1.title HOLA
+        p {{msg}}
+        router-link(:to="{name: 'login'}") login
 </template>
 
 <script>
@@ -19,12 +18,15 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="scss" scoped>
+
 h1,
 h2 {
   text-shadow: 4px 4px 4px #aaa;
   color: #2c3e50;
+
 }
 .container {
-  margin-top: 150px;
+  margin-top: 83px;
+  z-index: 1;
 }
 </style>
